87fc653bf1a956c577354f0cd4f04fa70943a6d3
twork7607-eng <twork7607@gmail.com>
2025-12-10 21:37:10 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = "https://vibevault-backend.onrender.com";
 export const currency = '$'
 
 const App = () => {
